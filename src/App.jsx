a19b3e56b403a998407ea383696ca5263432982b
Gboriss <galinaborissenko@gmail.com>
2020-02-04 22:33:04 +0300

    before eject react cli config

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,56 @@
 import React from 'react';
 
+
 function App() {
     return (
         <main>
-            <div className="containerName">
-                <div className="navigation">
-                    <div className="header">
-                    
+            <section className='sidebar'>
+                <h2 className='words'>words
+                    <div className='ui'>4</div>
+                </h2>
+                <h2 className='learn'>learn
+                    <div className='ui'>8</div>
+                </h2>
+                <h2 className='know'>know
+                    <div className='ui'>10</div>
+                </h2>
+            </section>
+
+            <form className='form-group'>
+                <input type='text' className='input-left' placeholder='Enter world'/>
+                <input type='text' className='input-right' placeholder='Translate'/>
+                <button type='button' className='buttonForm'>✔</button>
+            </form>
+            
+            <div className='cards'>
+                <div className='newspaper'>
+                    <h2 className='text'>fox</h2>
+                    <div className="back">
+                        <h2 className='translate'>лиса</h2>
+                        <button className='plus'>✓</button>
+                        <button className='minus'>×</button>
                     </div>
                 </div>
-            </div>
+
+                <div className='newspaper'>
+                    <h2 className='text'>dog</h2>
+                    <div className="back">
+                        <h2 className='translate'>пес</h2>
+                    <button className='plus'>✓</button>
+                    <button className='minus'>×</button>
+                     </div>
+                </div>
+            
+                <div className='newspaper'>
+                    <h2 className='text'>cat</h2>
+                    <div className="back">
+                        <h2 className='translate'>кошка</h2>
+                        <button className='plus'>✓</button>
+                        <button className='minus'>×</button>
+                    </div>
+                </div>
+            </div>  
+
         </main>
     )
 }
--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom'
 
 import App from './App'
 
+import s from '../src/img/1.png'
+
 
 ReactDOM.render(<App />, document.getElementById('root'))
